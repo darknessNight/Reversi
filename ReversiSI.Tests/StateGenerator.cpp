295548f6f846ab151914fc3e427f8fac44e64b65
Dominikus <dominikus-noah@live.de>
2017-04-19 14:18:23 +0200

    Fixed Test

--- ReversiSI.Tests/StateGenerator.cpp
+++ ReversiSI.Tests/StateGenerator.cpp
@@ -13,53 +13,53 @@ namespace SI::Reversi::Tests
 		//przyk³adowy test, który trzeba zmieniæ lub usun¹æ
 		TEST_METHOD(generatingStates_HasFirstStateAndPlayer1_CheckReturnAllNextStates)
 		{
-			MapState state;
-			state.SetFieldState(4, 4, MapState::State::Player1);				//		0 0 0 0
-			state.SetFieldState(3, 3, MapState::State::Player1);				//		0 1 2 0
-			state.SetFieldState(3, 4, MapState::State::Player2);				//		0 2 1 0
-			state.SetFieldState(4, 3, MapState::State::Player2);				//		0 0 0 0
-			StateGenerator generator(state, MapState::State::Player1);			
+			BoardState state;
+			state.SetFieldState(4, 4, BoardStateMemoryOptimized::State::Player1);				//		0 0 0 0
+			state.SetFieldState(3, 3, BoardStateMemoryOptimized::State::Player1);				//		0 1 2 0
+			state.SetFieldState(3, 4, BoardStateMemoryOptimized::State::Player2);				//		0 2 1 0
+			state.SetFieldState(4, 3, BoardStateMemoryOptimized::State::Player2);				//		0 0 0 0
+			StateGenerator generator(state, BoardStateMemoryOptimized::State::Player1);			
 																				
-			generator.GetAllNextStates(state, MapState::State::Player1);		
+			generator.GetAllNextStates(state, BoardStateMemoryOptimized::State::Player1);		
 																				
-			std::vector<MapState> result = std::vector<MapState>();
+			std::vector<BoardState> result = std::vector<BoardState>();
 			while ( generator.HasNextState() )
 				result.push_back(generator.GetNextState());
-			std::vector<MapState> expected = std::vector<MapState>();
+			std::vector<BoardState> expected = std::vector<BoardState>();
 
-			MapState expectedState;
-			expectedState.SetFieldState(4, 4, MapState::State::Player1);
-			expectedState.SetFieldState(4, 3, MapState::State::Player1);
-			expectedState.SetFieldState(4, 2, MapState::State::Player1);
-			expectedState.SetFieldState(3, 3, MapState::State::Player1);
-			expectedState.SetFieldState(3, 4, MapState::State::Player2);
+			BoardState expectedState;
+			expectedState.SetFieldState(4, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState.SetFieldState(4, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState.SetFieldState(4, 2, BoardStateMemoryOptimized::State::Player1);
+			expectedState.SetFieldState(3, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState.SetFieldState(3, 4, BoardStateMemoryOptimized::State::Player2);
 
 			expected.push_back(expectedState);
 
-			MapState expectedState2;
-			expectedState2.SetFieldState(4, 4, MapState::State::Player1);
-			expectedState2.SetFieldState(4, 3, MapState::State::Player2);
-			expectedState2.SetFieldState(2, 4, MapState::State::Player1);
-			expectedState2.SetFieldState(3, 3, MapState::State::Player1);
-			expectedState2.SetFieldState(3, 4, MapState::State::Player1);
+			BoardState expectedState2;
+			expectedState2.SetFieldState(4, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState2.SetFieldState(4, 3, BoardStateMemoryOptimized::State::Player2);
+			expectedState2.SetFieldState(2, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState2.SetFieldState(3, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState2.SetFieldState(3, 4, BoardStateMemoryOptimized::State::Player1);
 
 			expected.push_back(expectedState2);
 
-			MapState expectedState3;
-			expectedState3.SetFieldState(3, 3, MapState::State::Player1);
-			expectedState3.SetFieldState(4, 3, MapState::State::Player1);
-			expectedState3.SetFieldState(5, 3, MapState::State::Player1);
-			expectedState3.SetFieldState(4, 4, MapState::State::Player1);
-			expectedState3.SetFieldState(3, 4, MapState::State::Player2);
+			BoardState expectedState3;
+			expectedState3.SetFieldState(3, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState3.SetFieldState(4, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState3.SetFieldState(5, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState3.SetFieldState(4, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState3.SetFieldState(3, 4, BoardStateMemoryOptimized::State::Player2);
 
 			expected.push_back(expectedState3);
 
-			MapState expectedState4;
-			expectedState4.SetFieldState(3, 3, MapState::State::Player1);
-			expectedState4.SetFieldState(4, 3, MapState::State::Player2);
-			expectedState4.SetFieldState(3, 4, MapState::State::Player1);
-			expectedState4.SetFieldState(4, 4, MapState::State::Player1);
-			expectedState4.SetFieldState(3, 5, MapState::State::Player1);
+			BoardState expectedState4;
+			expectedState4.SetFieldState(3, 3, BoardStateMemoryOptimized::State::Player1);
+			expectedState4.SetFieldState(4, 3, BoardStateMemoryOptimized::State::Player2);
+			expectedState4.SetFieldState(3, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState4.SetFieldState(4, 4, BoardStateMemoryOptimized::State::Player1);
+			expectedState4.SetFieldState(3, 5, BoardStateMemoryOptimized::State::Player1);
 
 			expected.push_back(expectedState4);
 
@@ -70,7 +70,7 @@ namespace SI::Reversi::Tests
 
 		}
 
-		void MatchNextStates(std::vector<SI::Reversi::MapState> &expected, std::vector<SI::Reversi::MapState> &result)
+		void MatchNextStates(std::vector<SI::Reversi::BoardState> &expected, std::vector<SI::Reversi::BoardState> &result)
 		{
 			if ( expected.size() != result.size() )
 				Assert::Fail(L"Expected and result states number mismatch");
